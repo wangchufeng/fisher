8866d21ffbb299fbec7f61ac4b0bb3b5e2ff2eb9
GameChaner <941994464@qq.com>
2019-08-22 17:36:58 +0800

    start

---
+++ fisher.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')  # 载入config.py配置文件
+
+@app.route("/hello")
+def hello():
+	header = {
+		'content-type': 'text/plain', # 告诉浏览器怎么解析返回的文本
+		# 'location': 'http://www.baidu.com' # 重定向
+	}
+	#  返回（信息，状态码，头文件）
+	return "hello world", 200, header
+
+if __name__ == "__main__":
+	#  在生产环境下使用 nginx + uwsgi 来配置
+	#  uwsgi会加载这个入口文件
+	#  若没有写 if __name__ == "__main__":
+	#  则下面语句又会被执行，又会启动flask自带的服务器
+	app.run(host='0.0.0.0', debug = app.config['DEBUG'], port=5000)
